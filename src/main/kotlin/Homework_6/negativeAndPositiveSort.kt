cf2e7cc1d850a7ef5c826236cffcb1c519a32107
Dima <poznyak.dima2018@gmail.com>
2025-09-15 15:45:03 +0300

    result Homework6

--- src/main/kotlin/Homework_6/negativeAndPositiveSort.kt
+++ src/main/kotlin/Homework_6/negativeAndPositiveSort.kt
@@ -1,2 +1,22 @@
 package Homework_6
 
+fun negativeAndPositiveSort(array:IntArray){
+    for(i in 0..array.size-1){
+        for(j in 1..array.size-1){
+            if(array[j] <0 ){
+                val temp = array[j-1]
+                array[j-1] = array[j]
+                array[j] = temp
+            }
+        }
+    }
+
+    array.forEach {
+        println(it)
+    }
+}
+
+fun main(){
+    val array = intArrayOf(3,-5,5,-6,7,4,-8,-2)
+    negativeAndPositiveSort(array)
+}
