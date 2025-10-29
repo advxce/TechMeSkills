d7728f66ceb14ee1af59d2c2328d5ac8d0f0f468
Dima <poznyak.dima2018@gmail.com>
2025-10-29 12:55:04 +0300

    homework14 add

---
+++ src/main/kotlin/Homework_14/task1.kt
@@ -0,0 +1,51 @@
+package Homework_14
+
+val obj = Any()
+
+class Counter(){
+
+
+    var count = 0
+
+    fun increment(){
+        synchronized(obj){
+            count++
+        }
+    }
+
+    fun decrement(){
+        synchronized(obj){
+            count--
+        }
+    }
+
+}
+
+
+fun main(){
+
+    val counter = Counter()
+
+
+    val firstThread = Thread{
+        repeat(10){
+            counter.increment()
+        }
+    }
+
+    val secondThread = Thread{
+        repeat(10){
+            counter.decrement()
+        }
+    }
+
+    firstThread.start()
+    secondThread.start()
+
+    firstThread.join()
+    secondThread.join()
+
+    println(counter.count)
+
+
+}
---
+++ src/main/kotlin/Homework_14/task3.kt
@@ -0,0 +1,49 @@
+package Homework_14
+
+class NewCounter(){
+    var counter:Int = 0
+
+    fun increment(lock:Any){
+        synchronized(lock){
+            counter++
+        }
+    }
+
+    fun decrement(lock:Any){
+        synchronized(lock){
+            counter--
+        }
+    }
+}
+
+
+fun main(){
+    val counter = NewCounter()
+
+    val obj1 = Any()
+
+    val firstThread = Thread{
+
+        repeat(10){
+            println("Thread First")
+            counter.increment(obj1)
+//            Thread.sleep(500)
+        }
+    }
+
+    val secondThread = Thread{
+        repeat(10){
+            println("Thread Second")
+
+            counter.decrement(obj1)
+//            Thread.sleep(500)
+        }
+    }
+    firstThread.start()
+    secondThread.start()
+
+    firstThread.join()
+    secondThread.join()
+
+    println(counter.counter)
+}
